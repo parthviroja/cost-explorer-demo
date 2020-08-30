ca69cef9d53f5e5a689b197e3a1fdec67af5ff5c
Parth M. Viroja <parth.m.viroja@gmail.com>
2020-08-31 00:52:28 +0530

    update env

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ module.exports = {
         port: process.env.PORT || 5000
     },
     database: {
-        host: "13.232.112.67",
-        database: "demo",
-        user: "demo",
-        password: "123456",
-        port: "3306"
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USER || "demo",
+        password: process.env.DB_PASS || "123456",
+        database: process.env.DB_NAME || "demo"
     }
 };
